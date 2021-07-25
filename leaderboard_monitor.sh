b286869c0b0855ac3e1761650ab4ac5a19557719
Sergey <52304443+c29r3@users.noreply.github.com>
2021-07-25 04:19:30 +0300

    Update leaderboard_monitor.sh

--- leaderboard_monitor.sh
+++ leaderboard_monitor.sh
@@ -4,7 +4,8 @@ validator_address="B62qjhiEXP45KEk8Fch4FnYJQ7UMMfiR3hq9ZeMUZ8ia3MbfEteSYDg"
 TG_TOKEN=$1
 CHAT_ID=$2
 
-r=`curl -s 'http://uptime.minaprotocol.com/getPageData.php?pageNumber=1&_=1627162675974' \
+request() { 
+curl -s 'http://uptime.minaprotocol.com/getPageData.php?pageNumber=1' \
   -H 'Connection: keep-alive' \
   -H 'Accept: */*' \
   -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36' \
@@ -14,8 +15,11 @@ r=`curl -s 'http://uptime.minaprotocol.com/getPageData.php?pageNumber=1&_=162716
   -H 'Accept-Language: en,ru;q=0.9' \
   -H 'dnt: 1' \
   --compressed \
-  --insecure | grep -A 3 -B 3 $validator_address | grep 'td scope="row">' | egrep -o "[0-9]+"`
-previous_place=${r}
+  --insecure \
+  | grep -A 3 -B 3 $validator_address | grep 'td scope="row">' | egrep -o "[0-9]+"
+}
+
+previous_place=${request}
 
 
 send_message() {
@@ -27,9 +31,9 @@ send_message() {
 
 
 while true; do
-  sleep 300
+  sleep 120
   
-  new_place=$r
+  new_place=$request
   echo -e "previous_place=${previous_place} new_place=${new_place}"
   if [ $previous_place -gt  $new_place ]; then
     msg="We have lost $(echo ${previous_place}-${new_place} | bc) position(s). Current position is ${new_place}"
